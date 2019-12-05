573a8e2b59d17b955e6aad7aa079e8258eafa98d
valasulth <romain-culleron_student2019@wilder.school>
2019-12-05 17:55:57 +0100

    add basic css

---
+++ src/Controller/MeetingsController.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MeetingsController extends AbstractController
+{
+    /**
+     * @Route ("/meetings", name="show_meetings")
+     * @return Response A response instance
+     */
+    public function Meetings() :Response
+    {
+        return $this->render('meetings.html.twig');
+    }
+}
