c0b93aa6b011b29a953c85b581d004dee0b1dac1
LewisSama <lou.lesage09@gmail.com>
2019-12-06 14:14:05 +0100

    add fonts

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,9 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../scss/app.scss');
-
+require('../css/fonts.css');
 require('../css/display_list_style.css');
+
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
