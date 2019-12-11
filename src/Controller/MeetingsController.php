156b3a6b11d40ece7ba5cdffd96d691982ad6a0d
valasulth <romain-culleron_student2019@wilder.school>
2019-12-11 11:18:40 +0100

    base for the checkboxe and db.sql

--- src/Controller/MeetingsController.php
+++ src/Controller/MeetingsController.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace App\Controller;
-
+use App\Form\DateType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 
 class MeetingsController extends AbstractController
 {
@@ -12,8 +13,22 @@ class MeetingsController extends AbstractController
      * @Route ("/meetings", name="show_meetings")
      * @return Response A response instance
      */
-    public function meetings() :Response
+    public function meetings(HttpFoundationRequest $request) :Response
     {
-        return $this->render('meetings_display/meetings.html.twig');
+    $form = $this->createForm(
+            DateType::class
+    );
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid())
+    {
+        $date = $form->getData();
+        $entityManager = $this->getDoctrine()->getManager();
+        dump($entityManager);
+        $entityManager->persist($date);
+        $entityManager->flush();
+    }
+    return $this->render('meetings_display/meetings.html.twig', [
+        'form' => $form->createView()
+    ]);
     }
 }
