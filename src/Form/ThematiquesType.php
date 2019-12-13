9cce623fde57eec03273d411bd77b6e3ec6d1ff4
LewisSama <lou.lesage09@gmail.com>
2019-12-13 16:22:52 +0100

    adding style admin subjects

--- src/Form/ThematiquesType.php
+++ src/Form/ThematiquesType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Thematiques;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +15,7 @@ class ThematiquesType extends AbstractType
         $builder
             ->add('name')
             ->add('image')
-            ->add('description')
+            ->add('description', TextareaType::class)
         ;
         $options = [];
     }
