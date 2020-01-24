9fe44ce2b9c494f6ea81433ea18181907f6bc111
Lewïs <51699602+LewisSama@users.noreply.github.com>
2020-01-24 09:36:01 +0100

    Update UserType.php

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -18,7 +18,7 @@ class UserType extends AbstractType
     {
         $options;
         $builder
-            ->add('username')
+            ->add('username', null, 'label' => 'Nom d\'utilisateur',)
             ->add('roles', ChoiceType::class, [
                'label' => 'Nom d\'utilisateur',
                 'choices' => [
